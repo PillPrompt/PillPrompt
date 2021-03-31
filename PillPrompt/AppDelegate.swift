aaabc2d59f77bedf1743be119ce0c02222990d0b
evandilger <dilgere@purdue.edu>
2021-03-31 18:02:49 -0400

    Initialized PillPrompt workspace
    Created PillPrompt app workspace on Xcode and configured AppDelegate.swift for Parse API

---
+++ PillPrompt/AppDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  AppDelegate.swift
+//  PillPrompt
+//
+//  Created by Evan Dilger on 3/31/21.
+//
+
+import UIKit
+import Parse
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "oglK6bQkd8MCF2uBc13tyhomFl0dZb3dJHA0tnE6"
+                    $0.clientKey = "2PiOzjSCDpRR0eXzNi2ol4L0TN5sShO4Ly8MFARy"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'PillPrompt' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for PillPrompt
+  pod "AlamofireImage"
+  pod 'Parse'
+
+  target 'PillPromptTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'PillPromptUITests' do
+    # Pods for testing
+  end
+
+end
